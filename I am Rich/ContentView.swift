e679e5df78590a4d0b32ca85561da97401ba3c8a
Luis Vargas <luisvargasr10@gmail.com>
2020-11-27 12:43:21 -0600

    Initial Commit

---
+++ I am Rich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I am Rich
+//
+//  Created by Luis Vargas on 11/27/20.
+//  Copyright © 2020 Luis Vargas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
